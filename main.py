65ff0b44a4855fc21ed7b4ab634f47f97ebce24a
LordCatbug <gui.pegeot@gmail.com>
2024-06-26 12:12:36 +0200

    change to fit the intended usage : - azur icon + stackoverflow icon + change url to display the question to tagify + add url to just question the api

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from fastapi import FastAPI, Form, Request, status
-from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
+from fastapi import FastAPI, Request, Query
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse
 import uvicorn
-
+import torch
+import tensorflow as tf
+from transformers import BertTokenizer, TFBertForSequenceClassification
+import os
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
@@ -20,15 +25,16 @@ async def favicon():
     file_path = './static/' + file_name
     return FileResponse(path=file_path, headers={'mimetype': 'image/vnd.microsoft.icon'})
 
-@app.post('/hello', response_class=HTMLResponse)
-async def hello(request: Request, name: str = Form(...)):
-    if name:
-        print('Request for hello page received with name=%s' % name)
-        return templates.TemplateResponse('hello.html', {"request": request, 'name':name})
-    else:
-        print('Request for hello page received with no name or blank name -- redirecting')
-        return RedirectResponse(request.url_for("index"), status_code=status.HTTP_302_FOUND)
+@app.get('/hello', response_class=HTMLResponse)
+async def hello(request: Request, title: str = Query(...)):
+    print(f'Request for hello page received with title={title}')
+    return templates.TemplateResponse('hello.html', {"request": request, 'title': title})
+
+
+@app.get("/get-tags", response_class=JSONResponse)
+async def get_title(title: str = Query(...)):
+    tags = title.split(" ")
+    return {"title": title, "tags":tags}
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='0.0.0.0', port=8000)
-
